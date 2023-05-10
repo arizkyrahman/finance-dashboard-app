52c90d96f68041d9146b27df11a72f90cbf2e504
arizkyrahman <rizkyrahman7202@gmail.com>
2023-05-10 14:48:18 +0700

    create kpi route

---
+++ server/routes/kpi.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/kpis", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
