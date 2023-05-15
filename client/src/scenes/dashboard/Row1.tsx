5fd30c6fb7cc028072e111a7cd3261a40466b927
arizkyrahman <rizkyrahman7202@gmail.com>
2023-05-15 20:00:49 +0700

    revenue graph dev

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,5 +1,7 @@
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
+import { useTheme } from "@mui/material";
+import { useMemo } from "react";
 import {
   ResponsiveContainer,
   AreaChart,
@@ -13,6 +15,7 @@ import {
 type Props = {};
 
 const Row1 = (props: Props) => {
+  const { palette } = useTheme();
   const { data } = useGetKpisQuery();
   console.log("data", data);
   const revenueExpenses = useMemo(() => {
@@ -35,7 +38,7 @@ const Row1 = (props: Props) => {
           <AreaChart
             width={500}
             height={400}
-            data={data}
+            data={revenueExpenses}
             margin={{
               top: 10,
               right: 30,
@@ -43,15 +46,47 @@ const Row1 = (props: Props) => {
               bottom: 0,
             }}
           >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              tickLine={false}
+              axisLine={{ strokeWidth: "0" }}
+              style={{ fontSize: "10px" }}
+              domain={[8000, 23000]}
+            />
             <Tooltip />
             <Area
               type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fill="#8884d8"
+              dataKey="revenue"
+              dot={true}
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue)"
+            />
+            <Area
+              type="monotone"
+              dataKey="expenses"
+              dot={true}
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorExpenses)"
             />
           </AreaChart>
         </ResponsiveContainer>
